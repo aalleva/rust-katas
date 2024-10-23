2f3c63c89d3917649a8ca60c2a5fa5ba975e089e
Andres Alleva <andres.alleva@gmail.com>
2024-10-23 23:12:39 +0100

    Done with max_product implementation

--- maximum-product/src/main.rs
+++ maximum-product/src/main.rs
@@ -1,7 +1,20 @@
 fn max_product(string: &str, length: usize) -> u64 {
     let mut max_product = 0;
 
-    max_product = 72;
+    if string.len() < length {
+        return 0;
+    }
+
+    for i in 0..=string.len() - length {
+        
+        let slice = &string[i..i + length];
+        let product = slice.chars().fold(1, |acc, c| acc * c.to_digit(10).unwrap_or(0) as u64);
+
+        if product > max_product {
+            max_product = product;
+        }
+
+    }
 
     max_product
 }
@@ -17,8 +30,10 @@ mod tests {
     #[test]
     fn test_max_product() {
         assert_eq!(max_product("123456789", 2), 72);
-        //assert_eq!(max_product("73167176531330624919225119674426574742355349194934", 6), 23520);
-        //assert_eq!(max_product("12345", 3), 60);
-        //assert_eq!(max_product("123", 5), 0);
+        assert_eq!(max_product("73167176531330624919225119674426574742355349194934", 6), 23520);
+        assert_eq!(max_product("12345", 3), 60);
+        assert_eq!(max_product("123", 5), 0);
+        assert_eq!(max_product("1020304050607089", 2), 72);
+        assert_eq!(max_product("2,C,8,10", 2), 0);
     }
 }
